e0cce6bee3eecd95593db003887edfcb35bbdeb9
Brent-Kuhn <bkuhn4669@flpoly.org>
2018-10-04 10:32:51 -0400

    Finally fixed all the bugs in the code, reduced register use by using shifts instead of counters and mult

--- Lab_4/check_hex.asm
+++ Lab_4/check_hex.asm
@@ -31,14 +31,13 @@ main:
 	#li $v0, 11		#print the current character
 	#syscall
 	
-	li $t5, 1 		#assume the word is a hex word
 	
 checkWord:
-	lb $t3, 0($t2)		#load the first byte of the string
-	#lb $t4, ($t1)		#load the first byte of the string for incramenting
+	li $s2, 1 		#assume the word is a hex word
 	
-	li $t6, 0		#start a counter for the length of the substring
 	countWord:
+		lb $t3, 0($t2)		#load the first byte of the string
+		beq $t3, 0, exit	#if zero is found in the register, you are out of the string, so exit
 		beq $t3, '.', continue	#if t3 is a period, then the word is over, and check for state
 		beq $t3, ' ', continue	#if t3 is a space, then the word is over, and check for state
 		bgt $t3, 'F', notHex	#if t3 is not a hex, flip the flag and keep counting
@@ -46,63 +45,64 @@ checkWord:
 		j countWord		#jump back to countWord to finish checking the current word
 	
 		notHex:
-			li $t5, 0		#if t3 is not a hex, change the flag to zero for a not hex word
+			li $s2, 0		#if t3 is not a hex, change the flag to zero for a not hex word
 			add $t2, $t2, 1		#incrament t2 for the next letter
 			j countWord
 		
 	
 	continue:
+		lb $t3, 0($t1)		#load byte of the first char in the previously read string
 		beq $t3, '.', exit	#if t2 is a period, then exit
-		add $t2, $t2, 1		#incrament t3
-		beq $t5, 0, keepWord	#if t5 is zero then it was not a hex word
-		beq $t5, 1, replaceWord	#if t5 is one then it was a hex word
+		add $t2, $t2, 1		#incrament t2 to avoid circular logic of spaces
+		beq $s2, 0, keepWord	#if t5 is zero then it was not a hex word
+		beq $s2, 1, replaceWord	#if t5 is one then it was a hex word
 		
 		#add $t3, $t3, 1		#increase t3 by one byte
 
 keepWord:
 	beq $t1, $t2, checkWord	#return to checking letters
-	lb $t7, ($t1)	#load the byte of the start of the word
 	#print the word out by looping until space or period is found
-	move $a0, $t7
+	move $a0, $t3
 	li $v0, 11	#print the current character
 	syscall
 	add $t1, $t1, 1	#incrament t1
-	li $t5, 1 	#reset the assumption that the word is a hex word
+	lb $t3, 0($t1)	#load the next byte
 	j keepWord	#else keep printint the letters
 
 
 replaceWord:
-	beq $t1, $t2, countWord	#return to checking letters
-	lb $t7, ($t1)	#load the byte of the start of the word
-	sub $t6, $t6, 1	#decrament t6 by one
-	li $t8, 0
-	bge $t7, 65, makeInt
-	#print the word out by looping until space or period is found
+	beq $t3, ' ', printHex
+	beq $t3, '.', printHex
+	bge $t3, 'A', convertHexChar
+	subi $t3, $t3, 48	#convert to an integer value from ascii value
+	j storeHex
 	
-	
-	add $t1, $t1, 1	#incrament t1
-	
-	j replaceWord	#else keep printint the letters
-	
-	#possibly branch to print this?
-	move $a0, $t7
-	li $v0, 1	#print the current int
-	syscall
-	
-	makeInt:
-		sub $t7, $t7, 54	#make an intger equal to the hex value for this letter
-		ble $t8, $t6, baseSixteen #create the multiplier for the current number
+	convertHexChar:
+		subi $t3, $t3, 55	#convert to 
+		storeHex:
+			sll $t4, $t4, 4		#move the bytes over to place hex values in the proper place
+			add $t4, $t4, $t3	#Save the hex value in t4
+			addi $t1, $t1, 1	#incrament t1
+			lb $t3, 0($t1)		#load the next byte into t3
+			j replaceWord		#return to replace word
+	printHex:
+		move $a0, $t4
+		li $v0, 1		#print as an int
+		syscall
 		
-	
-		add $t1, $t1, 1	#incrament t1
-		beq $t1, $t2, countWord	#return to checking letters
-		j replaceWord	#else keep printint the letters
+		ble $t4, $s3, noNewMax
+		move $s3, $t4
 		
-		baseSixteen:
-			#loop through t8 to t6
-			#multiply 16 by its self that many times
-	
+		noNewHex:
+			li $t4, 0		#clean out t4
+			addi $t1, $t1, 1	#move t1 to the next byte
+			li $v0, 11		#print the space or period
+			move $a0, $t3
+			syscall
+			j countWord		#start checking the next word
 exit:
+	#branch if the conditions are met for string printing
+	#print the proper string
 	#Exit the program 
 	li $v0, 10
 	syscall
